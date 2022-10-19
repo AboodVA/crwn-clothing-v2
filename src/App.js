54e4bc378c521d1033d0bef0d8b9330dedb10444
Abdullah <Abdullah Al-araj@hotmail.com>
2022-10-19 21:32:51 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.styles.scss";
-import Category from "./category/Category";
+import Category from "./components/category/Category";
+import Home from "./components/routes/home/Home";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Category categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
